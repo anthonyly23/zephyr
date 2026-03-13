8c12ef35eab3bae44973c798c6ab8e55b48976c2
Laura Carlesso <laura.carlesso@infineon.com>
2026-03-12 19:56:15 -0700

    boards: infineon: pse84: Enable SRF in PSE84 boards when building with TF-M
    SRF is an Infineon library that allows the non-secure
    partition to send secure requests to the secure partition
    to execute. Secure aware PDL APIs require SRF to be
    properly invoked when security is in place which is the
    default case for the cm33/ns execution in Zephyr.
    The changes here enable SRF when building with TFM
    
    Signed-off-by: Laura Carlesso <laura.carlesso@infineon.com>

--- soc/infineon/edge/pse84/soc_pse84_m33_ns.c
+++ soc/infineon/edge/pse84/soc_pse84_m33_ns.c
@@ -16,11 +16,33 @@
 
 #define PSE84_CPU_FREQ_HZ DT_PROP(DT_PATH(cpus, cpu_0), clock_frequency)
 
+#if defined(CONFIG_BUILD_WITH_TFM)
+#include "mtb_srf_pool_init.h"
+#include "cy_syslib.h"
+
+mtb_srf_pool_t cy_pdl_srf_default_pool;
+CY_SECTION_SHAREDMEM _MTB_SRF_DATA_ALIGN uint32_t cy_pdl_srf_default_pool_memory[
+								(MTB_SRF_POOL_ENTRY_SIZE(
+								MTB_SRF_MAX_ARG_IN_SIZE,
+								MTB_SRF_MAX_ARG_OUT_SIZE)
+								* MTB_SRF_POOL_SIZE) /
+								sizeof(uint32_t)];
+#endif /* defined(CONFIG_BUILD_WITH_TFM) */
+
 
 void soc_early_init_hook(void)
 {
 	/* Initialize SystemCoreClock variable. */
 	SystemCoreClockSetup(PSE84_CPU_FREQ_HZ, PSE84_CPU_FREQ_HZ);
+
+#ifdef CONFIG_BUILD_WITH_TFM
+	mtb_srf_pool_init(&cy_pdl_srf_default_pool,
+						&cy_pdl_srf_default_pool_memory[0],
+						MTB_SRF_POOL_SIZE,
+						MTB_SRF_MAX_ARG_IN_SIZE,
+						MTB_SRF_MAX_ARG_OUT_SIZE);
+#endif /* CONFIG_BUILD_WITH_TFM */
+
 }
 
 void soc_late_init_hook(void)
